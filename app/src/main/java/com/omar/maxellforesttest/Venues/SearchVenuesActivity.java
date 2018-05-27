07cf79a3669a105b0cf57ac2cf02da3c4e629854
Omar Mujtaba <omar.mujtaba@gmail.com>
2018-05-27 23:09:56 +1000

    VenuesListFragment loaded via SearchVenuesActivity

--- app/src/main/java/com/omar/maxellforesttest/Venues/SearchVenuesActivity.java
+++ app/src/main/java/com/omar/maxellforesttest/Venues/SearchVenuesActivity.java
@@ -1,9 +1,12 @@
 package com.omar.maxellforesttest.Venues;
 
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 
 import com.omar.maxellforesttest.Common.ui.BaseActivity;
 import com.omar.maxellforesttest.R;
+import com.omar.maxellforesttest.Venues.fragments.VenuesListFragment;
 
 /**
  * Created by omz on 27/5/18
@@ -11,13 +14,39 @@ import com.omar.maxellforesttest.R;
 
 public class SearchVenuesActivity extends BaseActivity {
 
+    FragmentManager fragmentManager;
+
+    // Fragments and their tags variable which we will use to check whether a previous instance of the fragment has been created
+    private String TAG_VENUES_LIST = "vList";
+    private VenuesListFragment venuesListFragment;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        fragmentManager = this.getSupportFragmentManager();
+
+        loadFragment();
     }
 
     @Override
     public int getContentViewReference() {
         return R.layout.activity_venues_search;
+    }
+
+    private void loadFragment() {
+
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        venuesListFragment = (VenuesListFragment) fragmentManager.findFragmentByTag(TAG_VENUES_LIST);
+
+        if (venuesListFragment == null) {
+            venuesListFragment = new VenuesListFragment();
+            fragmentTransaction.add(R.id.fragmentContainer, venuesListFragment, TAG_VENUES_LIST);
+        } else {
+            fragmentTransaction.replace(R.id.fragmentContainer, venuesListFragment, TAG_VENUES_LIST);
+        }
+
+        fragmentTransaction.commitAllowingStateLoss();
     }
 }
--- app/src/main/java/com/omar/maxellforesttest/Venues/fragments/VenuesListFragment.java
+++ app/src/main/java/com/omar/maxellforesttest/Venues/fragments/VenuesListFragment.java
@@ -1,9 +1,31 @@
 package com.omar.maxellforesttest.Venues.fragments;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
 import com.omar.maxellforesttest.Common.ui.BaseFragment;
+import com.omar.maxellforesttest.R;
 
 /**
  * Created by omz on 27/5/18
  */
 public class VenuesListFragment extends BaseFragment {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Keep data saved while screen orientation changes
+        setRetainInstance(true);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_venues_list, container, false);
+
+        return view;
+    }
 }
