aa6ec3a5c203af6a7345702c13e7fed23803e2e6
omarthred <omarm@thred.im>
2018-05-28 11:16:41 +1000

    API creds updated

--- app/src/main/java/com/omar/maxellforesttest/Common/network/ServerSettings.java
+++ app/src/main/java/com/omar/maxellforesttest/Common/network/ServerSettings.java
@@ -13,8 +13,8 @@ public class ServerSettings {
         public static final HttpLoggingInterceptor.Level HTTP_LOG_LEVEL = HttpLoggingInterceptor.Level.BODY;
 
         public static final String BASE_URL = "https://api.foursquare.com/v2/";
-        public static final String BASE_URL_CLIENT_ID = "EXTVXYFQ50GXVOJMMA32FMWNWREEXONRU1RD3BFYJCQQTQ23";
-        public static final String BASE_URL_CLIENT_SECRET = "AVTGTXX1RYIMIHTJPWB3MB1RMCZBBZBDBWPV3FV0MREZ5FKX";
+        public static final String BASE_URL_CLIENT_ID = "0Q3WCEXHI130Z54K2VWIEA12LOVA4W3XYCIZOZSDTGBH3JJP";
+        public static final String BASE_URL_CLIENT_SECRET = "X44GFRLGLCJ5RHFOEHEGQ2WMRK3AK0VPEK2INAMWS3KYKRVD";
         public static final String BASE_URL_API_VERSION = "20180425";
 
     }
